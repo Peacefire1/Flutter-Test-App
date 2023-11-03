1c85498c50b3b4769145663e46afd78301c60f1f
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-11-03 09:10:00 +0800

    Test App

---
+++ lib/screens/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:movie/screens/movies.dart';
+import 'package:movie/screens/profile.dart';
+import 'package:movie/screens/wishlist.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  List<Widget> _totalPage = [MoviesPage(), WishListPage(), ProfilePage()];
+
+  void setCurrentIndex(int val) {
+    setState(() {
+      _currentIndex = val;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _totalPage[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: setCurrentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: "Movie",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
